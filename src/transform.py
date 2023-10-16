8c1a06b8d51aae989922b0079c6b4858c806e7b9
hicham atanane <hicham.atn@gmail.com>
2023-10-16 19:02:59 +0100

    create data classes for dimension tables and fact table

--- src/transform.py
+++ src/transform.py
@@ -4,3 +4,44 @@ from datetime import datetime
 from calendar import day_name
 import json
 import re
+
+
+@dataclass
+class DimensionTrendingDate:
+    trending_date: datetime
+    year: int
+    month: int
+    day_of_month: int
+    day_of_week: str
+    week_of_month: int
+    week_of_year: int
+
+
+@dataclass
+class DimensionVideo:
+    video_id: str
+    title: str
+    duration: int
+    caption: int
+    rank: int
+    published_at: str
+    description: str
+    tags: list[str]
+
+
+@dataclass
+class DimensionChannel:
+    channel_id: str
+    channel_title: str
+
+
+@dataclass
+class FactTrendingVideoStats:
+    trending_date_pk: int
+    video_pk: int
+    channel_pk: int
+    category_id: int
+    region_code: str
+    view_count: int
+    like_count: int
+    comment_count: int
