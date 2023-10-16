8283b27e7fea4f1fc7b86bcf27b0ae0306312d09
hicham atanane <hicham.atn@gmail.com>
2023-10-16 18:55:15 +0100

    create function region_codes to load defined region codes

--- src/extract.py
+++ src/extract.py
@@ -15,3 +15,15 @@ REGION_CODES_FILE = Path("../data/input/region_codes.txt")
 STAGING_DIR = "../data/staging/"
 LOG_FILE = Path(STAGING_DIR) / "etl.log"
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+
+
+def region_codes() -> list[str]:
+    try:
+        with open(REGION_CODES_FILE, "r", encoding="utf-8") as codes:
+            REGION_CODES = [code.rstrip("\n") for code in codes]
+        return REGION_CODES
+    except FileNotFoundError:
+        logging.error(
+            "Please make sure the region_codes.txt file exists in the provided path!"
+        )
+        sys.exit(1)
