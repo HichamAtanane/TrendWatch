9db5c763cc39bdca40c6a8ed15d7319e9e39ab9d
hicham atanane <hicham.atn@gmail.com>
2023-10-16 18:54:19 +0100

    add global variables to extract.py

--- src/extract.py
+++ src/extract.py
@@ -7,3 +7,11 @@ from datetime import datetime
 from typing import Optional
 from googleapiclient.discovery import build
 from dotenv import dotenv_values
+
+
+config = dotenv_values("../.env")
+API_KEY = config["API_KEY"]
+REGION_CODES_FILE = Path("../data/input/region_codes.txt")
+STAGING_DIR = "../data/staging/"
+LOG_FILE = Path(STAGING_DIR) / "etl.log"
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
