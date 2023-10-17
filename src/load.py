ac76bd2c81ee09abb76e7e5be4389ea876542fe4
hicham atanane <hicham.atn@gmail.com>
2023-10-17 11:14:10 +0100

    add main function to complete the loading

--- src/load.py
+++ src/load.py
@@ -41,3 +41,31 @@ def load_trending_video_stats(session, data: dict) -> None:
     session.add(trending_video_stats)
     session.commit()
     return None
+
+
+def main(session) -> None:
+    json_files = STAGING_DIR.glob("*_1.json")
+    for json_file in json_files:
+        data = read_json_file(json_file)
+        for video_data in data:
+            trending_date, dim_video, dim_channel, video_statistics = parse_video_data(
+                video_data, json_file
+            )
+            trending_date_pk = load_dim_trending_date(session, trending_date)
+            video_pk = load_dim_video(session, dim_video)
+            channel_pk = load_dim_channel(session, dim_channel)
+            video_statistics["trending_date_pk"] = trending_date_pk
+            video_statistics["video_pk"] = video_pk
+            video_statistics["channel_pk"] = channel_pk
+            trending_video_stats = parse_trending_video_stats(video_statistics)
+            load_trending_video_stats(session, trending_video_stats)
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"{DB}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    main(session)
